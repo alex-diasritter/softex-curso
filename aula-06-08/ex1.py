fe8716482eb76469a737e8a842b519c75c027896
alex-diasritter <ritter.alex@hotmail.com>
2025-08-06 15:00:31 -0300

    condicionais e operadores logicos

--- aula-06-08/ex1.py
+++ aula-06-08/ex1.py
@@ -20,3 +20,33 @@ if nmrInt % 5 == 0:
     print(f'{nmrInt} é divisível por 5!')
 else:
     print(f'{nmrInt} não é divisível por 5 :((( ')
+
+print('-------------------------------------------------------')
+
+n1 = int(input('digite um nmr inteiro: '))
+n2 = int(input('digite outro nmr inteiro: '))
+
+if n1 == n2:
+        print(f'os numeros são iguais')
+elif n1 > n2:
+        print(f'{n1} é maior que {n2}')
+else:
+    print(f'{n2} é maior que {n1}')
+
+print('-------------------------------------------------------')
+
+n = int(input('digite um nmr inteiro: '))
+if n % 2 == 0:
+    print(f'o número {n} é par.')
+else:
+     print(f'o nmr {n} é ímpar.')
+
+print('-------------------------------------------------------')
+
+age = int(input('digite a sua idade: '))
+if age >= 0 and age <= 12:
+     print(f'Criança: {age}')
+elif age >= 13 and age <= 17:
+     print(f'Adolescente: {age}')
+else:
+     print(f'Adulto: {age}')
