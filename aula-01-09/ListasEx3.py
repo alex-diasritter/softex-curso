925005446859496ef8d4e50df2543637a4a28b00
julianasoftex <227845472+julianasoftex@users.noreply.github.com>
2025-09-01 15:42:39 -0300

    Create ListasEx3.py
    feature: filtrando números primos

---
+++ aula-01-09/ListasEx3.py
@@ -0,0 +1,10 @@
+lista = [1,2,3,4,5,6,7,8,9,10,11,12]
+primos = []
+
+for numero in lista:
+    if numero == 2:
+        primos.append(numero)
+    for n in range(0, numero):
+        if numero % 2 == 1 and numero not in primos and numero != 1:
+            primos.append(numero)
+print(primos)
